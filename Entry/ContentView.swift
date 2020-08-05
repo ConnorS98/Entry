8d0690d42bf3f0d5c6ced0ae057473d5e2133653
ConnorS98 <connorhuffman16@gmail.com>
2020-08-05 15:50:36 -0500

    Setup NavBar

--- Entry/ContentView.swift
+++ Entry/ContentView.swift
@@ -9,9 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var items = [Item]()
+    @State var showCreateListPopover = false
+
     var body: some View {
-        Text("Hello, World!")
+        //1. Embed your View into a Navigation View
+        NavigationView {
+            List(items) { item in
+                Text(item.name)
+            }
+            //2. Add a Title for your Navigation Bar
+            .navigationBarTitle(Text("Entry"), displayMode: .inline)
+            //.3 Add the Bar Items
+            .navigationBarItems(leading:
+                Button(action: {self.addList()}) {
+                    Image(systemName: "person.crop.circle")
+                }, trailing:
+                Button(action: {print("HI")}) {
+                    Image(systemName: "square.and.pencil")
+                }
+            )
+        }
     }
+
+    func addList() {
+        
+    }
+}
+
+struct Item: Identifiable {
+    var id: Int
+    var name: String
 }
 
 struct ContentView_Previews: PreviewProvider {
