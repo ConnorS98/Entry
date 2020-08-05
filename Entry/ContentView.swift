6050b4d6b15684944506d49c8ef3546d923b78cd
ConnorS98 <connorhuffman16@gmail.com>
2020-08-05 13:30:12 -0500

    Initial Commit

---
+++ Entry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Entry
+//
+//  Created by Connor Huffman on 8/5/20.
+//  Copyright © 2020 Connor Huffman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
